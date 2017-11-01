2a6f5fe8794a64d245e16d0eff749f478115134a
AlexAnonymous101 <32960975+AlexAnonymous101@users.noreply.github.com>
2017-11-01 11:33:17 +0100

    Add files via upload

---
+++ website/displayimages.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gallery</title>
+</head>
+<body>
+	<p>Pictures, drawings, and vids</p>
+</body>
+</html>
---
+++ website/uploadfile.php
@@ -0,0 +1,34 @@
+<?php
+define("UPLOAD_DIR", "/srv/www/uploads/");
+
+if (!empty($_FILES["uploadfiles.php"])) {
+    $myFile = $_FILES["uploadfiles.php"];
+
+    if ($myFile["error"] !== UPLOAD_ERR_OK) {
+        echo "<p>An error occurred.</p>";
+        exit;
+    }
+
+    // ensure a safe filename
+    $name = preg_replace("/[^A-Z0-9._-]/i", "_", $myFile["website"]);
+
+    // don't overwrite an existing file
+    $i = 0;
+    $parts = pathinfo($name);
+    while (file_exists(UPLOAD_DIR . $name)) {
+        $i++;
+        $name = $parts["filename"] . "-" . $i . "." . $parts["extension"];
+    }
+
+    // preserve file from temporary directory
+    $success = move_uploaded_file($myFile["uploadfiles.php"],
+        UPLOAD_DIR . $name);
+    if (!$success) { 
+        echo "<p>Unable to save file.</p>";
+        exit;
+    }
+
+    // set proper permissions on the new file
+    chmod(UPLOAD_DIR . $name, 0644);
+
+?>
